34b2147686304355aa0c75e94c8daefaa7e68376
Станислав <morevstas02@mail.ru>
2025-09-13 23:40:02 +0300

    Области действия фикстур (scope)

--- test_shops/fixture/fix.py
+++ test_shops/fixture/fix.py
@@ -1,28 +1,39 @@
 import pytest
-import os
+# облость действие фикстуры scope
+@pytest.fixture(scope='session')
+def session_db_connection():
+    print('\n Фикстура session_db_connection: устанавливаю соединение с БД-1 раз за сессию')
+    connection = {'status': 'connected'} # Имитация соединения
+    yield connection
+    print('\n Фикстура session_db_connection: закрываю соединение с БД -один раз за сессию')
 
 
-@pytest.fixture()
-def temp_file_fixture():
-    file_path = 'temp_test_file.txt'
-    print(f'Фикстура: создаю файл {file_path}')
-    with open(file_path,'w') as f:
-        f.write('Hello, pytest!')
+# Фикстура выполняется один раз для каждого модуля.
+@pytest.fixture(scope='module')
+def module_data_setup(session_db_connection):
+    #эта фикстура зависит от session_db_connection
+    assert session_db_connection['status'] == 'connected'
+    print('\n Фикстура module_data_setup: настраиваю данные для модуля - Один раз за сессию')
+    data = {'module_id': 123}
+    yield data
+    print('\n Фикстура module_data_setup: ощищаю данные модуля')
 
-    yield file_path # тест получает это значение
+class TestUserOperations:
+    def test_user_create(self, module_data_setup, session_db_connection):
+        print("(Тест test_user_create)")
+        assert session_db_connection["status"] == "connected"
+        assert module_data_setup["module_id"] == 123
 
-    print(f'Фикстура удаляет файл {file_path}')
-    os.remove(file_path)
-
-
-def test_read_temp_file(temp_file_fixture):
-    file_path = temp_file_fixture # получаем путь к файлу от фикстуры
-    print(f'Тест чистаю файл {file_path}')
-    with open(file_path, 'r') as f:
-        content = f.read()
-    assert content == 'Hello, pytest!'
+    def test_user_view(self, module_data_setup, session_db_connection):
+        print("(Тест test_user_view)")
+        assert session_db_connection["status"] == "connected"
+        assert module_data_setup["module_id"] == 123
 
 
+def test_another_operation(module_data_setup, session_db_connection):
+    print("(Тест test_another_operation в том же модуле)")
+    assert session_db_connection["status"] == "connected"
+    assert module_data_setup["module_id"] == 123
 
 
 
